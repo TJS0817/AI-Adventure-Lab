25c76054a10ba95f054ee6f509f73e5ab6ab4274
TJS0817 <137964685+TJS0817@users.noreply.github.com>
2026-04-20 09:50:57 +0800

    Add mission hub home page

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import { AppShell } from "@/components/shell/app-shell";
+import { MissionHub } from "@/components/home/mission-hub";
+
+export default function Home() {
+  return (
+    <AppShell>
+      <MissionHub />
+    </AppShell>
+  );
+}
